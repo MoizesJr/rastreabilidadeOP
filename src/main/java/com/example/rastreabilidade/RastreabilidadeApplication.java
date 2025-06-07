5c43177ee46daaf80a2645f8306ecce1c09e8624
Moizés Júnior <128239325+MoizesJr@users.noreply.github.com>
2025-06-07 09:45:21 -0300

    Inicio projeto full stack: rastreabilidade

---
+++ src/main/java/com/example/rastreabilidade/RastreabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rastreabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreabilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreabilidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rastreabilidade/RastreabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rastreabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
