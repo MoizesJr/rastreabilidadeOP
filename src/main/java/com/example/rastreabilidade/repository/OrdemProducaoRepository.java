678a882e9721bca14417a87106c515b4744a9289
Moizés Júnior <128239325+MoizesJr@users.noreply.github.com>
2025-06-17 21:24:32 -0300

    .

--- src/main/java/com/example/rastreabilidade/repository/OrdemProducaoRepository.java
+++ src/main/java/com/example/rastreabilidade/repository/OrdemProducaoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.rastreabilidade.entity.OrdemProducao;
 
 public interface OrdemProducaoRepository extends JpaRepository<OrdemProducao, Long> {
+
 }
